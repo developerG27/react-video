ce4efc0cdf4dde2ff224e42d0d044ed692de05bd
DeveloperG27 <g.pumayalla@hotmail.com>
2018-10-03 15:22:30 +0200

    inserito logo

---
+++ src/pages/components/related.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../../images/logo.png';
+
+function Related(props){  
+  return(
+    <div>
+      <img src={logo} width={250} />
+    </div>
+  )
+}
+export default Related;
