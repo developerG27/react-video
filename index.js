dec2c25995dd6a0b222eb1e363710bac455c877e
DeveloperG27 <g.pumayalla@hotmail.com>
2018-10-03 09:45:10 +0200

    Eventi

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Media from './src/playlist/components/media';
+
+const app = document.querySelector('#app');
+// const hello = <h1>Hello world</h1>
+
+// ReactDOM.render(Cosa renderizzo, dove lo farò)
+// ReactDOM.render( hello , app);
+
+render(<Media type="video" title="Cos'è il responsive design?" author="GianPumayalla" images="./images/covers/bitcoin.jpg"/>, app)
+
+
+// npm run build:dev
---
+++ src/playlist/components/media.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './media.css';
+
+class Media extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      author: props.author
+    }
+  //   this.handleClick = this.handleClick.bind(this)
+  }
+  
+  // handleClick(event){
+  //   console.log(this.props.title)
+  // }
+
+  handleClick = (event) => {
+    // console.log(this.props.title)
+    // this.state.author = 'Riccardo'
+    this.setState({
+      author: 'RiccardoBianchi'
+    })
+  } 
+
+  render(){
+    /*const styles = {
+      container: {
+        // fontSize: 14,
+        // backgroundColor: 'white', 
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red  '
+      }
+    }*/
+    return(
+      <div className="Media" /*style = {styles.container}*/ onClick={this.handleClick} >
+        <div className="Media-cover">
+          <img 
+            src={this.props.images}
+            alt=""
+            width={260}
+            height={160}
+            className="Media-image"
+          />
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.state.author}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+Media.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string,
+  type: PropTypes.oneOf(['video','audio']),
+}
+
+export default Media;
