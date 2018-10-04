0d5bd95b802527559248f0a0974b37af9cd6f4cc
DeveloperG27 <g.pumayalla@hotmail.com>
2018-10-04 12:30:51 +0200

    creata la logica che prende la durata del video dai metadati

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 import './video.css';
 
 class Video extends Component {
-  togglePlay(){
-    if(this.props.pause){
+  togglePlay() {
+    if (this.props.pause) {
       this.video.play()
-    } else{
+    } else {
       this.video.pause()
     }
   }
-  componentWillReceiveProps(nextProps){
-    if(nextProps.pause !== this.props.pause){
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.pause !== this.props.pause) {
       this.togglePlay();
     }
   }
-  setRef = element =>{
+  setRef = element => {
     this.video = element;
   }
   render() {
+    const {
+      handleLoadedMetadata,
+    } = this.props;
+
     return (
       <div className="Video">
         <video
           autoPlay={this.props.autoplay}
           src={this.props.src}
           ref={this.setRef}
+          onLoadedMetadata={handleLoadedMetadata}
         />
       </div>
     )
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -3,34 +3,49 @@ import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
 import PlayPause from '../components/play-pause';
+import Timer from '../components/timer.js';
+import Controls from '../components/video-player-controls.js';
 
 class VideoPlayer extends Component {
   state = {
     pause: true,
+    duration: 0,
   }
   togglePlay = (event) => {
     this.setState({
       pause: !this.state.pause
     })
   }
-  componentDidMount(){
+  componentDidMount() {
     this.setState({
       pause: (!this.props.autoplay)
     })
+  }
+  handleLoadedMetadata = event => {
+    this.video = event.target;
+    this.setState({
+      duration: this.video.duration
+    });
   }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
-          title="Questo è il titolo del video!"
+          title="Questo è il titolo del video"
         />
-        <PlayPause
-          pause={this.state.pause}
-          handleClick={this.togglePlay}
-        />
+        <Controls>
+          <PlayPause
+            pause={this.state.pause}
+            handleClick={this.togglePlay}
+          />
+          <Timer
+            duration={this.state.duration}
+          />
+        </Controls>
         <Video
           autoplay={this.props.autoplay}
           pause={this.state.pause}
+          handleLoadedMetadata={this.handleLoadedMetadata}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>
