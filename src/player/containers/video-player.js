aa9c8ec3b0775587282f7c098fcaf09a8c4c0222
DeveloperG27 <g.pumayalla@hotmail.com>
2018-10-04 11:53:04 +0200

    aggiunta la logica degli svg play e pause

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -13,18 +13,24 @@ class VideoPlayer extends Component {
       pause: !this.state.pause
     })
   }
+  componentDidMount(){
+    this.setState({
+      pause: (!this.props.autoplay)
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
-          title="Esto es un video chido!"
+          title="Questo è il titolo del video!"
         />
         <PlayPause
           pause={this.state.pause}
           handleClick={this.togglePlay}
         />
         <Video
-          autoplay={true}
+          autoplay={this.props.autoplay}
+          pause={this.state.pause}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
         />
       </VideoPlayerLayout>
